5fec6af7e0d7ecef991496edc0f78e85961bf881
ygqyhhd <2210649752@qq.com>
2020-02-29 10:08:32 +0800

    Initial commit

---
+++ src/test/java/com/t248/ygq/mybatis/MybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.t248.ygq.mybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
